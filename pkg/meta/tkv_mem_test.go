2be2d338588eda53d276eb89b52ed3f279a10967
Maksim Zinal <maxzinal@yandex.ru>
2023-06-28 13:12:50 +0300

    mock mem test for comparison

---
+++ pkg/meta/tkv_mem_test.go
@@ -0,0 +1,38 @@
+/*
+ * JuiceFS, Copyright 2023 Juicedata, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//nolint:errcheck
+package meta
+
+import (
+	"testing"
+)
+
+func TestMemClient(t *testing.T) { //skip mutate
+	m, err := newKVMeta("memkv", "", testConfig())
+	if err != nil {
+		t.Fatalf("create meta: %s", err)
+	}
+	testMeta(t, m)
+}
+
+func TestMem(t *testing.T) { //skip mutate
+	c, err := newMockClient("")
+	if err != nil {
+		t.Fatal(err)
+	}
+	testTKV(t, c)
+}
